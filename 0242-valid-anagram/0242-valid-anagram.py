c4b0260a1a0594cee0f15ebbf8f4c13c208e99bb
Matthew <149197907+LoudShrimp@users.noreply.github.com>
2024-10-22 21:02:44 -0700

    Time: 16 ms (96.06%), Space: 16.7 MB (98.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,15 +2,12 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
+
         countS, countT = {}, {}
 
-        #create the hashMaps 
         for i in range(len(s)):
-            countS[s[i]] = countS.get(s[i], 0) + 1
-            countT[t[i]] = countT.get(t[i], 0) + 1
-
-        #compare the counts of each character in the hashMaps
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
         for c in countS:
             if countS[c] != countT.get(c, 0):
                 return False
