472ddb5deaefde4d5f2faad8b38cdfb04e2cb895
Matthew <149197907+LoudShrimp@users.noreply.github.com>
2025-01-19 21:39:08 -0800

    Time: 326 ms (8.02%), Space: 25.4 MB (19.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,22 +2,21 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-
+        
         rows, cols = len(grid), len(grid[0])
         visited = set()
         islands = 0
 
         def bfs(r, c):
-            q = collections.deque()
-            visited.add((r, c))
+            q = deque()
             q.append((r, c))
-
+            visited.add((r, c))
             while q:
                 row, col = q.popleft()
                 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
                 for dr, dc in directions:
                     newRow, newCol = row + dr, col + dc
-                    if (newRow in range(rows) and 
+                    if (newRow in range(rows) and
                         newCol in range(cols) and
                         grid[newRow][newCol] == "1" and
                         (newRow, newCol) not in visited):
@@ -29,4 +28,5 @@ class Solution:
                 if grid[r][c] == "1" and (r, c) not in visited:
                     bfs(r, c)
                     islands += 1
+
         return islands
