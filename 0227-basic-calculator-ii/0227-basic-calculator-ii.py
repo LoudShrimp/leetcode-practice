5e68303aacc33d8578828812b374bb529c0f1921
Matthew <149197907+LoudShrimp@users.noreply.github.com>
2025-01-26 18:22:26 -0800

    Time: 52 ms (81.25%), Space: 19.1 MB (64.98%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,19 +2,20 @@ class Solution:
     def calculate(self, s: str) -> int:
         if not s:
             return 0
-        
-        prevNum = currNum = result = 0
+
+        result = 0
+        prevNum, currNum = 0, 0
         prevOp = '+'
 
         s += 'e'
         for c in s:
-            #check if the c is not an operator
+            #check if c is not an operator
             if c.isdigit():
                 currNum = currNum * 10 + int(c)
             elif c == ' ':
                 continue
 
-            #check if the c is an operator
+            #check if c is an operator
             else:
                 if prevOp == '+':
                     result += currNum
@@ -27,7 +28,7 @@ class Solution:
                 if prevOp == '*':
                     result -= prevNum
                     result += prevNum * currNum
-                    
+
                     prevNum = prevNum * currNum
                 if prevOp == '/':
                     result -= prevNum
